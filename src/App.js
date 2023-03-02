6ca9885a722dc7c750fe3b0f75d0aac14abea1e6
Ishaa <ishaa481999@gmail.com>
2023-03-02 23:33:37 +0530

    react4

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import Contact from "./Component/Contact";
+import Home from "./Component/Home";
+import { BrowserRouter,Route,Routes } from "react-router-dom";
+import Table from "./Component/Table";
+import { useState } from "react";
+import ContextData from "./ContextData";
+import Edit from "./Component/Edit";
+function App() {
+  const[data,setData]=useState([{name :"John",age:23,course:"MERN",batch:"November"},{name :"Stacy",age:25,course:"MERN",batch:"November"},{name :"Sofia",age:23,course:"MERN",batch:"December"},{name :"Stephen",age:22,course:"MERN",batch:"October"},{name :"Lily",age:23,course:"MERN",batch:"November"},{name :"Harry",age:26,course:"MERN",batch:"September"},{name :"Ron",age:23,course:"MERN",batch:"November"}]);
+  
+  return (
+    <>
+  <BrowserRouter>
+  <Routes>
+    <Route path='/' element={<Home/>}/>
+    <Route path='/contact' element={<Contact/>}/>
+    <Route path='/student' element={
+      <ContextData.Provider value={{entries:data,updateFunction: setData}}>
+     <Table />
+      </ContextData.Provider>
+    }/>
+    <Route path='/editStudent' element={
+      <ContextData.Provider value={{entries:data,updateFunction:setData}}>
+      <Edit />
+      </ContextData.Provider>
+    }/>
+  </Routes>
+  </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
---
+++ src/Component/Home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Nav from './Nav'
+import './Style.css'
+export default function Home() {
+  return (
+    <>
+    <Nav />
+    <div className='home'>This is Home page</div>
+    </>
+  )
+}
